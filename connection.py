104351e18fa4d44bf03c508b0170a2a22f393270
Gustavo Gonzaga <gustavoggdev@gmail.com>
2020-12-13 15:52:53 -0300

    Create Table If Not Exists

---
+++ connection.py
@@ -0,0 +1,49 @@
+import pyodbc
+import sqlalchemy
+from sqlalchemy.engine.reflection import Inspector
+from sqlalchemy import Table, Column, Integer, String, MetaData
+
+def conn():
+    try:
+
+        strConn = "mssql+pyodbc://@DESKTOP-EF86IG0\\SQLEXPRESS/db_imea?Integrated Security=SSPI;driver=ODBC+Driver+17+for+SQL+Server"
+        #strConn = "mssql+pyodbc://@INT-TI01\\SQLEXPRESS/db_imea?Integrated Security=SSPI;driver=ODBC+Driver+17+for+SQL+Serve"
+
+        engine = sqlalchemy.create_engine(strConn)
+
+        return engine
+    except NameError:
+        print("Erro de conexão")
+
+def createTableSoja(engine):
+    inspector = Inspector.from_engine(engine)
+
+    if 'soja' not in inspector.get_table_names():
+        try:
+            meta = MetaData()
+
+            soja = Table(
+                'soja', meta,
+                Column('Medio_Norte', String),
+                Column('Nordeste', String),
+                Column('Noroeste', String),
+                Column('Norte', String),
+                Column('Oeste', String),
+                Column('Sudeste', String),
+                Column('Mato_Grosso', String),
+                Column('Centro_Sul', String),
+                Column('Periodo', String),
+                Column('Dia', String),
+                Column('Mes', String),
+                Column('NrMes', String),
+                Column('Ano', String),
+                Column('Arquivo', String)
+            )
+
+            meta.create_all(engine)
+
+            return True
+        except NameError:
+            return False
+    else:
+        return True
--- load.py
+++ load.py
@@ -1,37 +1,38 @@
-import pyodbc
-import sqlalchemy
 import pandas as pd
+from connection import conn, createTableSoja
 
-engine = sqlalchemy.create_engine(
-    "mssql+pyodbc://@INT-TI01\\SQLEXPRESS/db_imea?Integrated Security=SSPI;driver=ODBC+Driver+17+for+SQL+Server"
-)
+engine = conn()
 
-# Carrega os dados que já estão na tabela do BD
-query = "SELECT * FROM dbo.soja s (NOLOCK) ORDER BY s.Ano, s.NrMes"
-df_existents = pd.read_sql(sql=query, con=engine)
+if createTableSoja(engine):
 
-# Carrega os dados do arquivo 'transform.csv' que foi gerado na transformação (transform.py)
-df_newRegisters = pd.read_csv('transform.csv', sep=";")
+    # Carrega os dados que já estão na tabela do BD
+    query = "SELECT * FROM dbo.soja s (NOLOCK) ORDER BY s.Ano, s.NrMes"
+    df_existents = pd.read_sql(sql=query, con=engine)
 
-# Cria um dataframe fazendo o join entre os dois DFs
-df_final = df_newRegisters.merge(df_existents, how='left', on=['Periodo'])
+    # Carrega os dados do arquivo 'transform.csv' que foi gerado na transformação (transform.py)
+    df_newRegisters = pd.read_csv('transform.csv', sep=";")
 
-# Cria a lista 'novos_periodos' baseado do DF do JOIN filtrando o DF Right for nulo
-# Ou seja, trazendo registos que ainda não existem no BD
-# Salva os valores da coluna Periodo na variavel
-novos_periodos = df_final["Periodo"].loc[
-    df_final["Ano_y"].notna() == False
-].to_list()
+    # Cria um dataframe fazendo o join entre os dois DFs
+    df_final = df_newRegisters.merge(df_existents, how='left', on=['Periodo'])
 
-# Filtra os novos registros somente com o periodo da variavel acima (que ainda não existem no BD)
-df_newRegisters = df_newRegisters.loc[
-    df_newRegisters["Periodo"].isin(novos_periodos)
-]
+    # Cria a lista 'novos_periodos' baseado do DF do JOIN filtrando o DF Right for nulo
+    # Ou seja, trazendo registos que ainda não existem no BD
+    # Salva os valores da coluna Periodo na variavel
+    novos_periodos = df_final["Periodo"].loc[
+        df_final["Ano_y"].notna() == False
+    ].to_list()
 
-# Se o DataFrame após filtro acima não for vazio, insere no BD
-if df_newRegisters.empty == False:
-    df_newRegisters[[
-        'Medio_Norte', 'Nordeste', 'Noroeste',
-           'Norte', 'Oeste', 'Sudeste', 'Mato_Grosso', 'Centro_Sul', 'Periodo',
-           'Dia', 'Mes', 'NrMes', 'Ano', 'Arquivo'
-    ]].to_sql('soja', con=engine, if_exists='append', index=False)
+    # Filtra os novos registros somente com o periodo da variavel acima (que ainda não existem no BD)
+    df_newRegisters = df_newRegisters.loc[
+        df_newRegisters["Periodo"].isin(novos_periodos)
+    ]
+
+    # Se o DataFrame após filtro acima não for vazio, insere no BD
+    if df_newRegisters.empty == False:
+        df_newRegisters[[
+            'Medio_Norte', 'Nordeste', 'Noroeste',
+            'Norte', 'Oeste', 'Sudeste', 'Mato_Grosso', 'Centro_Sul', 'Periodo',
+            'Dia', 'Mes', 'NrMes', 'Ano', 'Arquivo'
+        ]].to_sql('soja', con=engine, if_exists='append', index=False)
+else:
+    print("Não foi possível localizar ou criar a tabela no DW.")
